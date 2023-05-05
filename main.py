fd62b39de1f641074b7d47ff56be9e895344f0dc
Denis <denisring@protonmail.com>
2023-05-05 12:19:46 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import plotly.express as px
+import re
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+st.title("Diary Tone")
+
+
+st.subheader("Positivity")
+
+
+
+st.subheader("Negativity")
